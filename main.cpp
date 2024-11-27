d66963acddad2a11891ef9046a9b0ae42bbb4be5
jjj333_p <jjj333.p.1325@gmail.com>
2024-11-27 12:06:47 -1000

    breadth first search works almost perfectly
    still some edge cases, but made a huge difference

--- main.cpp
+++ main.cpp
@@ -197,39 +197,38 @@ void option3(quadtree *tree) {
     std::cout << "The closest city to (" << lat << ", " << longe << ") is... ";
 
     //breadth first search
-    std::stack<quadtree *> city_stack;
-    city_stack.push(tree);
+    std::queue<quadtree *> city_queue;
+    city_queue.push(tree);
 
     // float best_lat_difference = std::abs(tree->this_el->latitude - lat);
     // float best_longe_difference = std::abs(tree->this_el->longitude - longe);
     float best_distance = geo_to_mile(lat, longe, tree->this_el->latitude, tree->this_el->longitude);
     state *best_city = tree->this_el;
 
-    while (!city_stack.empty()) {
-        quadtree *city_subtree = city_stack.top();
+    while (!city_queue.empty()) {
+        quadtree *city_subtree = city_queue.front();
         if (!city_subtree) break; //the while loop should take care of this, but I don't trust
-        city_stack.pop();
+        city_queue.pop();
 
         //get current distance
         const float curr_distance = geo_to_mile(lat, longe, city_subtree->this_el->latitude,
                                                 city_subtree->this_el->longitude);
 
-        std::cout << std::endl << city_subtree->this_el->name << std::endl;
-
-        if (curr_distance < best_distance) {
+        //if at a better distance or on the first item
+        if (curr_distance < best_distance || city_subtree == tree) {
             //update bests
             best_city = city_subtree->this_el;
             best_distance = curr_distance;
 
             //check all elements if we're moving in the right direction
             if (city_subtree->nw)
-                city_stack.push(city_subtree->nw);
+                city_queue.push(city_subtree->nw);
             if (city_subtree->ne)
-                city_stack.push(city_subtree->ne);
+                city_queue.push(city_subtree->ne);
             if (city_subtree->se)
-                city_stack.push(city_subtree->se);
+                city_queue.push(city_subtree->se);
             if (city_subtree->sw)
-                city_stack.push(city_subtree->sw);
+                city_queue.push(city_subtree->sw);
 
             //return to the stack
             continue;
@@ -241,7 +240,7 @@ void option3(quadtree *tree) {
                                             city_subtree->nw->this_el->longitude)
             )
         ) {
-            city_stack.push(city_subtree->nw);
+            city_queue.push(city_subtree->nw);
         }
 
         //if ne exists and is going back in the right direction
@@ -250,7 +249,7 @@ void option3(quadtree *tree) {
                                             city_subtree->ne->this_el->longitude)
             )
         ) {
-            city_stack.push(city_subtree->ne);
+            city_queue.push(city_subtree->ne);
         }
 
         //if sw exists and is going back in the right direction
@@ -259,7 +258,7 @@ void option3(quadtree *tree) {
                                             city_subtree->sw->this_el->longitude)
             )
         ) {
-            city_stack.push(city_subtree->sw);
+            city_queue.push(city_subtree->sw);
         }
 
         //if se exists and is going back in the right direction
@@ -268,7 +267,7 @@ void option3(quadtree *tree) {
                                             city_subtree->se->this_el->longitude)
             )
         ) {
-            city_stack.push(city_subtree->se);
+            city_queue.push(city_subtree->se);
         }
     }
 
